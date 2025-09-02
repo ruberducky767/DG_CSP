4901a8009ba3ae6a81c28dd45eb5f84a0781e7c7
rubberducky767 <dominik.garces@ucas-edu.net>
2025-09-02 13:44:49 -0600

    coded variables practice

---
+++ notes/variables.py
@@ -0,0 +1,6 @@
+name = input("what is your name?\n")
+
+quest = input("what is your quest: ")
+
+print("what is the average volocity of an inlaiden swallow")
+volocity = input()
---
+++ practices/variables_practes.py
@@ -0,0 +1,25 @@
+# DG 6th Variables Practice
+
+name = input("what is your name\n")
+
+print("Hello", name)
+
+number1 = input ("pick an number betwen 1 and 10")
+
+number2 = input ("pick a number betwen 100 and 1000")
+
+breakfast = input ("what did you have for breakfast")
+
+color = input ("what is your favorite color")
+
+school = input ("what school do you go to")
+
+year = input ("what year is it")
+
+eyecolor = input ("what is your eyecolor")
+
+old = input ("how old are you")
+
+schoolsubject = input ("what is youre favorite school subject")
+
+print( name, " youre numder betwen 1 and 10 was",number1," youre number betwen 100 and 1000 is",number2,"you had", breakfast,"for breakfast")
