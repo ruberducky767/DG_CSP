e19a2ec7cac95c89253b30a03bd3482c02a574fa
ruberducky767 <dominik.garces@ucas-edu.net>
2025-09-03 01:38:55 +0000

    finnished

--- practices/variables_practes.py
+++ practices/variables_practes.py
@@ -2,24 +2,25 @@
 
 name = input("what is your name\n")
 
-print("Hello", name)
+print("Hello",name)
 
-number1 = input ("pick an number betwen 1 and 10")
+number1 = input ("pick an number between 1 and 10\n")
 
-number2 = input ("pick a number betwen 100 and 1000")
+number2 = input ("pick a number between 100 and 1000\n")
 
-breakfast = input ("what did you have for breakfast")
+breakfast = input ("what did you have for breakfast\n")
 
-color = input ("what is your favorite color")
+color = input ("what is your favorite color\n")
 
-school = input ("what school do you go to")
+school = input ("what school do you go to\n")
 
-year = input ("what year is it")
+year = input ("what year is it\n")
 
-eyecolor = input ("what is your eyecolor")
+eyecolor = input ("what is your eyecolor\n")
 
-old = input ("how old are you")
+old = input ("how old are you\n")
 
-schoolsubject = input ("what is youre favorite school subject")
+schoolsubject = input ("what is your favorite school subject\n")
 
-print( name, " youre numder betwen 1 and 10 was",number1," youre number betwen 100 and 1000 is",number2,"you had", breakfast,"for breakfast")
+print( " hello",name,"your number between 1 and 10 was",number1,"your number between 100 and 1000 is",number2,"you had",breakfast,"for breakfast \n and your favorite color is", 
+color,"you go to",school,"and the year is", year,"your eye color is",eyecolor,"you are",old,"years old and your favorite subject is",schoolsubject, )
