b95bcc8b1c383c98a96e2f4f1a329d0b1ac7e101
rubberducky767 <dominik.garces@ucas-edu.net>
2025-09-08 13:44:43 -0600

    happy

---
+++ notes/string_notes.py
@@ -0,0 +1,32 @@
+#DG 6th string notes
+#What makes something a string?
+# a string is made of () and ""and '' but it can only be ""or'' it cant be 
+#Why do we have strings?
+#we use strings to ask questions or print something
+#How do stupid proofing and sanitization relate to each other?
+
+#What is debugging?
+#How do you debug the different types of errors?
+#Describe what each of the methods below does:
+#find()
+#concatenate (add)
+#upper()
+#lower()
+#strip()
+
+
+
+
+
+# examples of strings
+first_name = input("what is your first name:\n").strip().title()
+
+last_name = input("what is your last name:\n").strip().title()
+
+full_name = first_name +" "+ last_name
+
+sentence = "the quick brown fox jumps over the lavy dog."
+
+print("welcome to my programe", full_name+"!")
+#stupid profing striping
+#.lower wil make it lower cais
