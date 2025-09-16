3d4464fd7934c076603774ffe5cac2f58b96798b
ruberducky767 <dominik.garces@ucas-edu.net>
2025-09-16 01:41:12 +0000

    hi

---
+++ practices/new_finance_calculator.py
@@ -0,0 +1,16 @@
+name = input("Hello welcome to the finance calculator what is your name\n").title().strip()
+print ("Welcome " + name)
+income =float(input(f"what is your monthly income"))
+rent =float(input(f"what is your cost rent"))
+utilities=float(input(f"what is your montly utilites bill"))
+groceres =float(input(f"what is your monthly groceres"))
+transportation=float(input(f"what is the your transportation cost"))
+
+def percent(type, amount):
+    per=amount/income *100
+    print(f"your {type} is {per}% income")
+
+print("rent",rent)
+print("utilities",utilities)
+print("groceres",groceres)
+print("transportation",transportation)
