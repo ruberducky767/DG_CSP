880f664ca2c8ebefce2ab31757aa25adabdb8aa7
ruberducky767 <dominik.garces@ucas-edu.net>
2025-09-19 00:33:15 +0000

    finnished old enough

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -1,14 +1,16 @@
-#DG 6th Old enough larose
-name = input("what is your name\n")
+#DG 6th Old enough LaRose
+name = input("what is your name\n").title().strip()
 
 age = int(input("how old are you\n"))
-
-if age >= 18:
-    print("you can vote")
+if age >= 100:
+    print(f"wow {name} you are old enough to be in the museum")
+elif age >= 18:
+    print("you can vote. be carfull its a big world out there")
 elif age >= 16:
-    print("you can drive by your self")
+    print("you can drive by your self. be freeee")
 elif age >= 15:
-    print(" you can get a learners permit")
+    print(" you can get a learners permit. spread your wings and dont crash")
+elif age >= 4:
+    print("you can go to school. the road is open now its your turn to choose your path")
 else:
-    if age >= 18:
-        print("you can vote")
+    print(f"{name} you should be using your imagination >:(")
