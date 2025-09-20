17091f7c1d35db800a1eff4f0638c570704290f2
ruberducky767 <dominik.garces@ucas-edu.net>
2025-09-20 19:03:49 +0000

    finnished time of day

--- practices/good_day.py
+++ practices/good_day.py
@@ -3,11 +3,12 @@
 import datetime
 current = datetime.datetime.now()
 hour = current.hour
-hour_of_day= (f"the current hour in millitary time is{hour}")
-if hour <= 9:
-    print("good morning")
-elif hour <= 12:
-    print("good afternoon")
-elif hour <= 17:
+print (f"the current hour in millitary time is{hour}")
+if hour >= 21:
+    print("good night")
+elif hour >= 17:
     print("good evening")
-elif hour <= 21:
+elif hour >= 12:
+    print("good good afternoon")
+elif hour >= 9:
+    print("good night")
