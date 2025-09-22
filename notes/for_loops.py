990d2912303259700c8d672c6e5f6cb707faa1f1
rubberducky767 <dominik.garces@ucas-edu.net>
2025-09-22 13:41:11 -0600

    looooooooooooooooooooooooooooooooooooooooooooooooooooooooooop

--- notes/for_loops.py
+++ notes/for_loops.py
@@ -10,4 +10,54 @@ print(f"the time is: {current}")
 print(f"the hour is {hour}")
 
 #lists
-sidlings = ["dominik", "xander", "adalynn"]
+siblings = ["dominik", "xander", "adalynn","cow"]
+print(siblings[1])
+print(siblings)
+siblings[0]= "dom"
+siblings.remove("cow")
+print(siblings)
+
+
+#for loop
+for sibling in siblings:
+    #print(f"hello {sibling}")
+    print("hi")
+
+for x in range(1,1000,10):
+    print(x)
+
+#while loops
+#infinint loops
+#while True:
+    #print("oh no!!!!")
+
+
+
+#good boy
+i = 2
+
+
+while i < 21:
+    if i % 2 == 0:
+        print(f"{i}is even")
+    else:
+        print(f"{i} is odd")
+    i +=1
+
+
+import random 
+
+number = random.randint(1,20)
+x=1
+"""while x != number:
+    print("duck")
+    x += 1
+print("goose!")"""
+
+while True:
+    if number == x:
+        print("goose!")
+        break
+    else:
+        print("duck")
+        x += 1
