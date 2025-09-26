fa0b7c24163bbabc53ec60138f79181bea15cfc3
rubberducky767 <dominik.garces@ucas-edu.net>
2025-09-26 13:44:16 -0600

    fhsgjhs fjghfdsjgh dsfg s

---
+++ practices/test.py
@@ -0,0 +1,17 @@
+#dg
+print("you find a jungle temple do you want to go around")
+yesno= input("yes or no ")
+          
+if yesno == "yes": 
+    print("you find a giant spider")
+
+    print("fight or run")
+    fightrun=input("fight or run")
+    if yesno == "yes":
+        input("stab or slash")
+        stab=input("stab or slash")
+        if stab=="stab":
+            print("you get a key")
+
+else:
+    print("you fell")
