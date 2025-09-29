32e7ea8734c95c9e363bc58da708dc300a9fe0d0
rubberducky767 <dominik.garces@ucas-edu.net>
2025-09-29 13:43:58 -0600

    made some crazy changes

--- practices/test.py
+++ practices/test.py
@@ -5,13 +5,27 @@ yesno= input("yes or no ")
 if yesno == "yes": 
     print("you find a giant spider")
 
-    print("fight or run")
-    fightrun=input("fight or run")
-    if yesno == "yes":
-        input("stab or slash")
+    
+    fightrun=input("run or fight")
+    if fightrun == "fight":
         stab=input("stab or slash")
-        if stab=="stab":
-            print("you get a key")
+        stab=="stab"
+        if stab == 'stab':
+             print("The spider, wounded backs into its web to see another day and amidst the temple you see a key on the ground\n")
+             key = print("you go inside the temple and see a key hole on the ground")
+             if key == 'unlock':
+                   print("you unlock it and you go down a hall and see a gigantic spider behind it you see the golden egg")
+                   ask=print('do you ask or fight')
+                   if ask=='ask':
+                         print("she picks you up and you die")
+                         if ask =='fight':
+                               print()
+
+        if stab == 'slash':
+              print('you made the spider mad')
+            #hp - charge
+    elif fightrun =="run":
+               print("The spider begins to chase. But since you are far slower than the spider she catches you and peirces your body with a burning venom.")
 
 else:
-    print("you fell")
+    print("you walk off of a cliff and fall to your doom")
