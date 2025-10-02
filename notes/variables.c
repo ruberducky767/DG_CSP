a79aa3e8f477b40f0e4be2f0c592769a9d3f8b16
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-02 13:44:12 -0600

    c

---
+++ notes/variables.c
@@ -0,0 +1,19 @@
+// DG 6th Variables notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A';//char will only hold one letter
+    char name[]="dominik";
+    //bool passing = true;
+    printf("%d", num);
+
+    return 0;
+}
+
+//num=4  int and :
+//int is what data it will hold
+//string= 32 byt
+//python is dynamice not static
+//; acts as a period
