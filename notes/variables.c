b3df074fe673e834fbbaf553dadc1f41ef303f51
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-03 13:44:01 -0600

    ahdsgfkj daf

--- notes/variables.c
+++ notes/variables.c
@@ -2,12 +2,25 @@
 #include <stdio.h>
 
 int main(void){
-    int num = 4;
-    float pi = 3.14;
-    char grade = 'A';//char will only hold one letter
-    char name[]="dominik";
+    int num ;
+    const float pi = 3.14;
+    char grade;//char will only hold one letter
+    char name[20];
     //bool passing = true;
+    printf("what is your letter grade: ");
+    scanf("%c", &grade);
+
+    printf("tell me a number: ");
+    scanf("%d", &num);
+
+
+    //printf("%d\n", num)
+   // printf("%s has a %c grade in class")
+    printf("what is your name: ");
+    fgets(name, sizeof(name), stdin);
+
     printf("%d", num);
+    printf("%s has a %c grade in the class!\n", name, grade);
 
     return 0;
 }
@@ -17,3 +30,8 @@ int main(void){
 //string= 32 byt
 //python is dynamice not static
 //; acts as a period
+// the main difference in c and python is
+//a int is 4 a float is 4 byte double is 8byte and a char is 1 byte
+// a string is a list of charicters
+// float is f int is d byte is c and double is lf
+/*python is the only code that doesnt let us do multi line comments*/
