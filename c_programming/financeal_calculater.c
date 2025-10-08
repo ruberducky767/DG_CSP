104fb227d481317601ad356ebe8e87e69868feba
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-08 13:43:49 -0600

    finance

---
+++ c_programming/financeal_calculater.c
@@ -0,0 +1,24 @@
+//financeal calculator DG 6th
+#include <stdio.h>
+
+int main(void){
+    float income;
+    printf("what is your income: ");
+    scanf("%f", &income);
+    printf("your income is %.2f\n", income );
+    float rent;
+    printf("what is your rent: ");
+    scanf("%f", &rent);
+    printf("your rent is %.2f\n", rent);
+    float utilities;
+    printf("what is your utilities: ");
+    scanf("%f", &utilities);
+    printf("your utilities is %.2f\n", utilities);
+    float grocerys;
+    printf("what is your grocerys");
+    scanf("%f", &grocerys);
+    printf("your grocerys is %.2f", grocerys);
+
+return 0;
+}
+
