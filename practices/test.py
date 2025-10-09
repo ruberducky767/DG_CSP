b15db4df0d69ebd217edb7dabc3e10d8cfbb8f05
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-09 19:30:04 +0000

    larose

--- practices/test.py
+++ practices/test.py
@@ -17,12 +17,15 @@ if yesno == "yes":
                    print("you unlock it and you go down a hall and see a gigantic spider behind it you see the golden egg")
                    ask=print('do you ask or fight')
                    if ask=='ask':
-                         print("she picks you up and you die")
+                         print("the spider picks you up and throws you acrose the room.") #hp - charge?
                          if ask =='fight':
-                               print()
+                              itamtake= print("you can use an item from your bage")
+                              if itamtake == 'yes':
 
-        if stab == 'slash':
-              print('you made the spider mad')
+                               
+
+                               if stab == 'slash':
+                                    print('you made the spider mad')
             #hp - charge
     elif fightrun =="run":
                print("The spider begins to chase. But since you are far slower than the spider she catches you and peirces your body with a burning venom.")
