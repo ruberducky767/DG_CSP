63b89bba48b6c4a22d5d368ed7ee83710de29e3e
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-09 13:43:47 -0600

    name decor

---
+++ c_programming/name_decorator.c
@@ -0,0 +1,12 @@
+//DG 6th name decoder
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char name[100];
+    printf("please tell me your name: ");
+    scanf("%s", name);
+    char decor[100];
+    printf("type random letters numbers in a pattern: ");
+    scanf("%s", name);
+    return 0;
+}
