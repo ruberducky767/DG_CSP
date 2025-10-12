754ae9ac5d5aebd4e8c277f77bca925e10151f57
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-12 19:07:10 +0000

    cow

--- c_programming/name_decorator.c
+++ c_programming/name_decorator.c
@@ -5,9 +5,8 @@ int main(void){
     char name[100];
     printf("please tell me your name: ");
     scanf("%s", name);
-    char decor[100];
-    printf("type random letters numbers in a pattern: ");
-    scanf("%s", decor);
-    printf("%s",name, "%s");
+    printf("*****%s*****\n", name);
+    printf("!?!?%s?!?!\n", name);
+    printf("~~__~~%s~~__~~\n", name);
     return 0;
 }
