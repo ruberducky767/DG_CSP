02e6d3121aabc679b5bd238e9feb4d9eeced5964
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-12 22:50:07 +0000

    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ c_programming/first_progame.c
@@ -0,0 +1,9 @@
+//DG 6th fisrt program
+#include <stdio.h>
+int main() {
+    char name [20];
+    printf("what is your name: ");
+    scanf("%20s", name);
+    printf("Hello, %s welcome to my first c code.\n",name);
+    return 0;
+}
