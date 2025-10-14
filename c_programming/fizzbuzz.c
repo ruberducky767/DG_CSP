5cd1ca165d896f012953d977b0f662678b9d5dc2
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-14 00:53:42 +0000

    FIIIZZZZBUUUUZZZZZZZ

--- c_programming/fizzbuzz.c
+++ c_programming/fizzbuzz.c
@@ -1,6 +1,20 @@
 //Dg 6th FizzBuzz
 #include <stdio.h>
 int main(){
+    for(int i=1; i<=50; i++){
+        if(i%3==0 && i%5==0){
+            printf("FizzBuzz!!!\n");
+        }
+        else if(i%3==0){
+            printf("fizz\n");
+        }
+        else if(i%5==0){
+            printf("buzz\n");
+        }
+        else{
+            printf("%d\n", i);
+        }
+    }
     
     return 0;
 }
