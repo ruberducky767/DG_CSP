3888059add8c8ea6aafd54cc4fddf8c24ac08f55
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-15 13:35:18 -0600

    hello world

---
+++ c_programming/hello_world.c
@@ -0,0 +1,21 @@
+//DG 6th update hello world
+#include <stdio.h>
+void name(char* name){
+    printf("hello %s\n",name);
+    
+
+    
+
+}
+int main(void){
+    name("Dominik");
+    name("Xander");
+    name("Adalynn");
+    name("Laticia");
+    name("Roberth");
+
+
+
+
+    return 0;
+}
