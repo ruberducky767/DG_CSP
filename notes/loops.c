d9d1d0242a2b45f9c38a68e0a6a956a7032cbf43
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-21 01:41:41 +0000

    looooooooooppppp

--- notes/loops.c
+++ notes/loops.c
@@ -1,6 +1,11 @@
 //DG 6th loop and arrays notes
+// a loop is a continus cycal in a peace of code
+// a for loop and a while loop
+// an itiration is a process of reapeting instructions in code
+//a collection of things in a list to reapet
+// while or for
 #include <stdio.h>
-
+//for loop
 int main(void){
     int nums[]={4,384,3,34,3,4,34,2,43};
     char candy[5][20]={"Skittles","butterfingers","rees's","twix","kitkat"};
@@ -15,7 +20,7 @@ int main(void){
         printf("%d\n", num);
     }
 
-
+//while loop
     int goose = 6;
     int count = 0;
     while(count != goose){
