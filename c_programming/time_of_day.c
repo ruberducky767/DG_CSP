d67388626e331d9e608d8265c142470715795168
ruberducky767 <dominik.garces@ucas-edu.net>
2025-10-21 01:20:51 +0000

    coowww

--- c_programming/time_of_day.c
+++ c_programming/time_of_day.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+
 int main(void){
-    printf("hi");
+    int mil_time;
+    printf("What is the time of day in military time (ex 09:30 or 15:30)\n");
+    if (scanf("%d", &mil_time) != 1) {
+    }
+
+    if (mil_time < 0 || mil_time > 2359) {
+        printf(" military time error. learn how to read a clock solder!!: %d\n", mil_time);
+    }
+
+    printf("Ok so the hour is %4d\n",mil_time);// i dont know why it keeps doing double space
+    printf("good job solder.\n");
     return 0;
 }
